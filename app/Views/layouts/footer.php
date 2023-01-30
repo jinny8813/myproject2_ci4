f83cad62a46dae372aca97d0646e54a11c9828e4
chingyi lan <jinny8813@gmail.com>
2023-01-30 18:25:18 +0800

    stylesheet 放最下面蓋過 bootstrap

--- app/Views/layouts/footer.php
+++ app/Views/layouts/footer.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Document</title>
-    <link rel="stylesheet" href="<?= base_url('../../public/assets/css/styles/style.css') ?>" />
+
     <link
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css"
       rel="stylesheet"
@@ -17,7 +17,7 @@
       integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8"
       crossorigin="anonymous"
     ></script>
-    <link rel="stylesheet" href="./styles/style.css" />
+    <link rel="stylesheet" href="<?= base_url('../../public/assets/css/styles/style.css') ?>" />
   </head>
 
   <body>
